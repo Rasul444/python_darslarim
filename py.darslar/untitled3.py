aea5fe575ee37805a24b872b247c4eea83516dc7
Rasul444 <123662156+Rasul444@users.noreply.github.com>
2023-01-27 14:34:20 +0500

    yangi faylar qushildi
    1,2,3,4 ta darslar qushildi

---
+++ py.darslar/untitled3.py
@@ -0,0 +1,78 @@
+#                                                 lugat
+# car_0 = {'model':'ferrari','rang':'qizil'}
+# car_0 = {'model':'ferrari','rang':'qizil'}
+
+# print(car_0['model'])
+# print(car_0['rang'])
+
+
+# talaba_0 = {'ism':'murod olimov','yosh':20,'t_yil':2000}
+# print(f"{talaba_0['ism'].title()},\
+#  {talaba_0['t_yil']}-yilda tu'gilgan,\
+#  {talaba_0['yosh']} yoshda")
+
+
+# talaba_0['kurs'] = 4 # yangi, 'kurs' nomli kalit so'zga 4 qiymatini yuklaymiz
+# talaba_0['fakultet'] = 'informatika' # 'fakultet' ga esa 'informatika' 
+# print(talaba_0)
+
+# talaba_1 = {}
+
+# talaba_1['ism'] = 'qobil rasulov'
+# talaba_1['kurs'] = 3
+# talaba_1['yosh'] = 20
+# print(talaba_1)
+# print(f"Talaba {talaba_1['ism'].title()} {talaba_1['kurs']}-kurs")
+
+
+# talaba_1['kurs'] = 4 # 'kurs' ni 4 ga o'zgartiramiz.
+# print(f"Talaba {talaba_1['ism'].title()} {talaba_1['kurs']}-kurs")
+
+
+# talaba_0 = {'ism':'murod olimov','yosh':20,'t_yil':2000}
+# print(talaba_0)
+# del talaba_0['yosh'] # yosh degan kalit so'z (va qiymatni) o'chiramiz
+# print(talaba_0)
+
+# telefonlar = {
+#     'ali':'iphone x',
+#     'vali':'galaxy s9',
+#     'olim':'mi 10 pro',
+#     'orif':'nokia 3310'
+#     }
+
+
+# phone = telefonlar['ali']
+# print(f"Alining telefoni {phone}")
+
+
+# phone = telefonlar['hasan']
+# print(f"Hasanning telefoni {phone}")
+
+
+# phone = telefonlar.get('hasan','Bunday ism mavjud emas')
+
+
+# print(phone)
+
+
+# phone = telefonlar.get('hasan')
+# print(phone)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ untitled2.py
@@ -0,0 +1,173 @@
+#                                             if elife else
+
+# yosh = int(input('Yoshingiz nechida? '))
+# if yosh<=4:
+#     print('Sizga kirish bepul.')
+# elif yosh<=12:
+#     print('Sizga kirish 5000 so\'m')
+# else:
+#     print('Sizga kirish 10000 so\'m')
+    
+    
+    
+# yosh = int(input('Yoshingiz nechida? '))
+# if yosh<=4:
+#     price = 0
+# elif yosh<=12:
+#     price = 5000
+# else:
+#     price = 10000
+    
+# print(f"Sizga kirish {price} so'm")
+    
+    
+# yosh = int(input('Yoshingiz nechida? '))
+# if yosh<=4: # yosh bolalarga bepul
+#     price = 0
+# elif yosh<=12: # 4 dan 12 yoshgacha 5000 so'm
+#     price = 5000
+# elif yosh<65: # 12 dan katta va 65 dan kichiklarga narh 10000 so'm
+#     price = 10000
+# else: # qariyalarga esa 8000 so'm
+#     price = 8000
+# print(f"Sizga kirish {price} so'm")
+    
+
+    
+# yosh = int(input('Yoshingiz nechida? '))
+# if yosh<=4:
+#     price = 0
+# elif yosh<=12:
+#     price = 5000
+# elif yosh<65:
+#     price = 10000
+# elif yosh>=65:
+#     price = 8000    
+# print(f"Sizga kirish {price} so'm")
+    
+    
+# kun = input("Bugun nima kun?>>>")
+# if kun.lower()=='shanba' or kun.lower()=='yakshanba':
+#     print('Bugun dam olish kuni.')
+# else:
+#     print('Bugun ish kuni.')
+    
+    
+# kun = input("Bugun nima kun?")
+# harorat = float(input("Havo harorati qanday?"))
+# if kun.lower()=='shanba' or kun.lower()=='yakshanba' and harorat>=30:
+#     print("Cho'milgani ketdik!")
+# elif kun.lower()=='shanba' or kun.lower()=='yakshanba' and harorat<30:
+#     print("Uyda dam olamiz!")   
+    
+    
+# narh = 15000 # mijoz 15000 so'mga taom oldi.
+# choy = True # mijoz choy ham oldi
+# salat = False # mijoz salat olmadi
+
+# if choy and salat: # agar mijoz choy ham salat ham olgan bo'lsa
+#     narh = narh + 10000 # narhga 10000 so'm qo'shamiz
+# elif choy or salat: # agar choy yoki salat olgan bo'lsa
+#     narh = narh + 5000 # narhga 5000 so'm qo'shamiz
+
+# print(f"Jami {narh} so'm") # yakuniy narhni chiqaramiz
+    
+    
+# narh = 15000 # mijoz 15 so'mga ovqat oldi
+# choy = True
+# salat = False
+# non = True
+# kompot = True
+# assorti = False
+# #Quyidagi har bir shart alohida tekshiriladi va bir-biriga bog'liq emas
+# if choy:   # agar choy olsa
+#     print("Mijoz choy oldi.")
+#     narh = narh + 3000
+# if salat:  # agar salat olsa
+#     print("Mijoz salat oldi.")
+#     narh = narh + 5000
+# if non:    # agar non olsa
+#     print("Mijoz non oldi.")
+#     narh = narh + 2000
+# if kompot: # agar kompot olsa
+#                                             if elif else
+
+
+
+#     print("Mijoz kompot oldi.")
+#     narh = narh + 5000
+# if assorti: # agar assorti olsa
+#     print("Mijoz assorti oldi.")
+#     narh = narh + 15000
+    
+# print(f"Jami {narh} so'm")
+    
+    
+# menu = ['osh','qazonkabob','shashlik','norin','somsa']
+# buyurtmalar = ["osh","somsa","manti", "shashlik"]
+
+# for taom in buyurtmalar:
+#     if taom in menu:
+#         print(f"Menuda {taom} bor")
+#     else:
+#         print(f"Kechirasiz, menuda {taom} yo'q")
+    
+    
+# menu = ['osh','qazonkabob','shashlik','norin','somsa']
+# buyurtmalar = ["osh","somsa","manti", "shashlik"]
+
+# if buyurtmalar: # ro'yxatda biror element bo'lsa bu ifoda TRUE qaytaradi
+#     for taom in buyurtmalar:
+#         if taom in menu:
+#             print(f"Menuda {taom} bor")
+#         else:
+#             print(f"Kechirasiz, menuda {taom} yo'q")
+# else: # agar ro'yxat bo'sh bo'lsa
+#     print("Savatchangiz bo'sh!")
+    
+    
+# menu = ['osh','qazonkabob','shashlik','norin','somsa']
+# 'manti' in menu # menu da manti bormi?
+    
+    
+# menu = ['osh','qazonkabob','shashlik','norin','somsa']
+# 'osh' in menu # menu da osh bormi?
+    
+    
+# menu = ['osh','qazonkabob','shashlik','norin','somsa']
+# ovqat = input('Nima ovqat yeysiz?>>>')
+# if ovqat.lower() in menu:
+#     print('Buyurtma qabul qilindi.')
+# else:
+#     print('Afsuski bizda bunday ovqat yo\'q')
+    
+    
+    
+# menu = ['osh','qazonkabob','shashlik','norin','somsa']
+# 'manti' not in menu # menu da manti yo'qmi?
+    
+    
+# menu = ['osh','qazonkabob','shashlik','norin','somsa']
+# 'osh' not in menu # menu da osh yo'qmi?
+    
+    
+    
+# menu = ['osh','qazonkabob','shashlik','norin','somsa']
+# ovqat = input('Nima ovqat yeysiz?>>>')
+# if ovqat.lower() not in menu:
+#     print('Afsuski bizda bunday ovqat yo\'q')
+# else:
+#     print('Buyurtma qabul qilindi.')
+    
+# menu = ['osh','qazonkabob','shashlik','norin','somsa']
+# buyurtmalar = ["osh","somsa","manti", "shashlik"]
+
+# for taom in buyurtmalar:
+#     if taom in menu:
+#         print(f"Menuda {taom} bor")
+#     else:
+#         print(f"Kechirasiz, menuda {taom} yo'q")
+    
+    
+    
+    
